e997329611384b17d11570c13aa847a382c7d69a
Cemal Bayrı <cemalbayri@gmail.com>
2023-09-12 21:32:38 +0300

    first commit

---
+++ Tests/SwiftExtTests/SwiftExtTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftExt
+
+final class SwiftExtTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftExt().text, "Hello, World!")
+    }
+}
