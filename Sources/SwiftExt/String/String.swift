671a7caff4e9809f066fe31dd6f39aa75e8017d3
Cemal Bayrı <cemalbayri@gmail.com>
2023-09-12 22:56:34 +0300

    string going on.

--- Sources/SwiftExt/String/String.swift
+++ Sources/SwiftExt/String/String.swift
@@ -9,18 +9,37 @@ import Foundation
 
 extension String {
     // MARK: - Add
-    public mutating func addFirst(char: Character) -> String {
-        self.insert(char, at: self.index(startIndex, offsetBy: .zero))
+    public mutating func addFirst(_ value: String) -> String {
+        self.insert(contentsOf: value, at: self.index(startIndex, offsetBy: .zero))
         return self
     }
     
-    public mutating func addLast(char: Character) -> String {
-        self.insert(char, at: self.index(startIndex, offsetBy: self.count))
+    public mutating func addLast(_ value: String) -> String {
+        self.insert(contentsOf: value, at: self.index(startIndex, offsetBy: self.count))
         return self
     }
     
-    public mutating func add(char: Character, index: Int) -> String {
-        self.insert(char, at: self.index(startIndex, offsetBy: index))
+    public mutating func add(_ value: String, index: Int) -> String {
+        self.insert(contentsOf: value, at: self.index(startIndex, offsetBy: index))
         return self
     }
-}
+    
+    //MARK: - Remove
+    public mutating func removeFirst() -> String {
+        let newText = self.dropFirst()
+        return String(newText)
+    }
+    
+    public mutating func removeLast() -> String {
+        let newText = self.dropLast()
+        return String(newText)
+    }
+    
+    public mutating func removeBefore(_ index: Int) -> String {
+        return String(self.suffix(index))
+    }
+    
+    public mutating func removeAfter(_ index: Int) -> String {
+        return String(self.prefix(index))
+    }
+ }
